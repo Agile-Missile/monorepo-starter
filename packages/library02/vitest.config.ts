380de9028c03955fbe4e61251e12e1998e3eca93
shunquan.wang <shunquan.wang@ly.com>
2024-09-10 21:36:46 +0800

    feat: perfect

--- packages/library02/vitest.config.ts
+++ packages/library02/vitest.config.ts
@@ -1,33 +1,11 @@
-import { swcUnpluginTs } from 'swc-unplugin-ts';
-import tsConfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [tsConfigPaths(), swcUnpluginTs.vite({})],
-  resolve: {
-    // https://github.com/aleclarson/vite-tsconfig-paths/issues/54
-    alias: [
-      // handle `@/*.js`, Do not use this alias, causeof `https://github.com/dividab/tsconfig-paths/pull/213` has been fixed.
-      // { find: /^(@\/.*)\.js$/, replacement: '$1.ts' },
-    ],
-  },
-  cacheDir: '../../.cache/vitest/forge',
+  plugins: [tsconfigPaths()],
   test: {
-    // Makebe suite for local debug
-    testTimeout: 50000000,
     globals: true,
-    environment: 'node',
-    passWithNoTests: false,
-    coverage: {
-      provider: 'istanbul',
-      reporter: ['text', 'clover'],
-      extension: ['js', 'jsx', 'ts', 'tsx'],
-    },
-    exclude: [
-      '**/node_modules/**',
-      'dist/**',
-      '**/coverage/**',
-      '**/.{idea,git,cache,output,temp}/**',
-    ],
+    exclude: [...configDefaults.exclude],
+    include: ['**/?(*.){test,spec}.?(c|m)[jt]s?(x)'],
   },
 });
